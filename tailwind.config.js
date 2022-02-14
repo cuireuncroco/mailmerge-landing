10fb1a74511968d08cc3a442e48d9edc4a8c877a
cuireuncroco <46222664+cuireuncroco@users.noreply.github.com>
2022-02-15 00:06:23 +0100

    feat: extend theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./_includes/**/*.html', './_layouts/**/*.html', './_posts/*.md', './*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Poppins': ['Poppins'],
+      },
+      colors: {
+        'primary-blue': {
+          100: '#D2E3FC',
+          500: '#1a73e8',
+        },
+        'primary-green': {
+          100: '#CEEAD6',
+          500: '#34a853',
+        },
+      },
+    },
   },
   plugins: [],
 }
